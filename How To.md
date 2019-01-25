e7e0f41b3c11d06c3ae359d109c17cc4d7682d1a
vitaliy <vtravelvitaliy@gmail.com>
2019-01-25 18:21:14 +0200

    les1

--- How To.md
+++ How To.md
@@ -4,10 +4,17 @@ create-react-app MyProjectName
 
 ## **Started**:
 npm run start; npm start
+
 Runs the app in the development mode.
 Open http://localhost:3000 to view it in the browser.
 
 
 ## **Structure:**
 index.js - точка входа
+
+
+## **react**
+
+function = () => ( выполняем и выводим что то ) то же, что 
+function(){return}
 
--- less1/src/App.js
+++ less1/src/App.js
@@ -1,16 +1,81 @@
 import React, { Component } from 'react';
 
+/*import my css*/
+import './App.css';
 /*css framework*/
 import 'normalize.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+/*import my json file*/
+import names from './data/names';
+
+console.log(names);
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello</h1>
+//       </div>
+//     );
+//   }
+// }
+
+
+class NamesList extends Component{
+
+    /*Импортим внешние данные */
+    state = {
+        users: names
+    }
+
+
+    filterUsers = (e) => {
+        console.log('TIC: ', e.target.value);
+
+        let val = e.target.value.toLowerCase();
+
+        let filterData = names.filter(nameUser=> {
+           // переводим в нижний регистр
+            let nameUserLowerCase = nameUser.name.toLowerCase();
+            if (nameUserLowerCase.indexOf(val) !== -1) {
+                return nameUser;
+            }
+
+        })
+
+        this.setState({
+            users: filterData
+        })
+    }
+
+    render = ()=>
+        <div className="wrap">
         <h1>Hello</h1>
-      </div>
-    );
-  }
+            <input  onChange={this.filterUsers} type="text" name="SearchUser" id="" placeholder='search to user'/>
+            {
+                this.testF()
+            }
+        </div>
+
+
+    testF = () => {
+        const { users } = this.state
+
+        return (
+            users.map((nameUser, key) => {
+                console.log(nameUser);
+                return (
+                    <p key={nameUser.name}>{nameUser.name}</p>
+
+                )
+            })
+        )
+
+    }
+
 }
 
-export default App;
+// export default App;
+
+export default NamesList;
